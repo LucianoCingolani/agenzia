a4a1da6081fad73279d7561989bfaad02bf7497c
Luciano Cingolani <lulucingolani@gmail.com>
2026-03-06 11:51:37 -0300

    TRUSTED ORIGINS

--- core/settings.py
+++ core/settings.py
@@ -119,3 +119,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TRUSTED_ORIGINS = env.list('TRUSTED_ORIGINS', default=[])
